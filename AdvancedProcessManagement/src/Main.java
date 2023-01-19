b0baccdbc4a8aed60df8a1f2c9995564276f8930
sperez-s <sps169@outlook.es>
2023-01-19 15:21:47 +0100

    double cat working

--- AdvancedProcessManagement/src/Main.java
+++ AdvancedProcessManagement/src/Main.java
@@ -1,59 +1,69 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
         int numComands = 0;
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        BufferedReader output;
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
         File inputFile = new File("input.txt");
-        File outputFile = new File("output.txt");
+        FileReader reader = null;
+        try {
+            reader = new FileReader(inputFile);
+        } catch (FileNotFoundException e) {
+            System.err.println("Couldn't find input file");
+        }
         ProcessBuilder pb1 = new ProcessBuilder();
         ProcessBuilder pb2 = new ProcessBuilder();
+        Process process1 = null;
         Process process2 = null;
         while (numComands < 2) {
             String[] command = {};
             try {
-                command = input.readLine().trim().split(" ");
+                command = keyboard.readLine().split(" ");
             }catch (IOException e) {
                 System.err.println(e.getMessage());
             }
             if (numComands % 2 == 0){
+                System.out.println("First command setup");
                 pb1.command(command);
             } else {
+                System.out.println("Second command setup");
                 pb2.command(command);
             }
             numComands++;
         }
         try {
-            input.close();
+            keyboard.close();
         }catch (IOException e) {
             System.err.println(e.getMessage());
         }
-        pb1.redirectInput(inputFile);
-        pb1.redirectOutput(pb2.redirectInput());
-        pb2.redirectOutput(outputFile);
+
         try {
-            pb1.start();
+            process1 = pb1.start();
             System.out.println("Process 1 started");
             process2 = pb2.start();
             System.out.println("Process 2 started");
+
+            if (reader != null) {
+                while (reader.ready()) {
+                    process1.getOutputStream().write(reader.read());
+                    System.out.print(".");
+                }
+                process1.getOutputStream().flush();
+                System.out.println("\nFile read into process 1 input");
+            }
+            while (process1.getInputStream().available() > 0) {
+                process2.getOutputStream().write(process1.getInputStream().read());
+                System.out.print(".");
+            }
+            process2.getOutputStream().flush();
+            System.out.println("\nProcess 1 wrote all bytes");
+            System.out.println("\nProcess 2 output:\n");
+            while (process2.getInputStream().available() > 0) {
+                System.out.print((char)process2.getInputStream().read());
+            }
+            System.out.println();
         }catch (Exception e){
             System.err.println("Wrong command input: " + e.getMessage());
         }
-        if (process2 != null)
-            try {
-                output = new BufferedReader(new InputStreamReader(process2.getInputStream()));
-                System.out.println("Reading process 2 output");
-                while(output.ready()) {
-                    System.out.println(output.readLine());
-                }
-                input.close();
-                output.close();
-            }catch (IOException e) {
-                System.err.println(e.getMessage());
-            }
     }
 }
